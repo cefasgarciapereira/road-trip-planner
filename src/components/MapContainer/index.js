daea5e6bc551a4dbb444973f9cb483eaf23b94cb
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-09-05 00:42:24 -0300

    Implements MapContainer

---
+++ src/components/MapContainer/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Map from '../Map';
+
+const googleMapsApiKey = "AIzaSyBC-9Mp5zO3H8t2E9uBIbYfDS2OzHVxl_w";
+
+const MapContainer = (props) => {
+  const {places, loadingElement,containerElement,mapElement,defaultCenter,defaultZoom} = props;
+
+  return (
+    <Map
+      googleMapURL={
+        'https://maps.googleapis.com/maps/api/js?key=' +
+        googleMapsApiKey +
+        '&libraries=geometry,drawing,places'
+      }
+      markers={places}
+      loadingElement={loadingElement || <div style={{height: `100%`}}/>}
+      containerElement={containerElement || <div style={{height: "80vh"}}/>}
+      mapElement={mapElement || <div style={{height: `100%`}}/>}
+      defaultCenter={defaultCenter || {lat: 25.798939, lng: -80.291409}}
+      defaultZoom={defaultZoom || 11}
+    />
+  );
+};
+
+export default MapContainer;
