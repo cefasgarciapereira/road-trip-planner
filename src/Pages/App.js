1dfe9ece4450546eb904b5f6a40124deb0fba350
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-09-05 00:43:52 -0300

    Set Up main Page

---
+++ src/Pages/App.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useContext} from 'react';
+import Form from '../components/Form';
+import TripContext from '../contexts/trip';
+import './app.css'
+
+function App() {
+  const {mapComponent} = useContext(TripContext)
+  
+  useEffect(() =>{
+    /*axios.get('https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=-21.7695566,-46.5547852&destinations=-23.5505199,-46.63330939999999&key=AIzaSyBC-9Mp5zO3H8t2E9uBIbYfDS2OzHVxl_w')
+    .then(response => console.log('Rotas',response))
+    .catch(error => console.log('Rotas',error))*/
+  },[])
+
+  return (
+    <div className='app'>
+      <Form/>
+      {mapComponent}
+    </div>
+  );
+}
+
+export default App;
